aa977c10a3352c36dd311f1e70ec9dc8d417059c
Josh Long <josh@joshlong.com>
2020-03-28 18:15:52 -0700

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -19,10 +19,19 @@ cf set-env ${APP_NAME} COVIDDB_PW   ${COVIDDB_PW}
 cf set-env ${APP_NAME} COVIDDB_USER ${COVIDDB_USER}
 cf set-env ${APP_NAME} COVIDDB_NAME ${COVIDDB_NAME}
 cf set-env ${APP_NAME} COVIDDB_HOST ${COVIDDB_HOST}
-
 cf set-env ${APP_NAME} SPRING_PROFILES_ACTIVE cloud
 cf set-env ${APP_NAME} JBP_CONFIG_OPEN_JDK_JRE '{ jre: { version: 11.+}}'
+
 cf restart ${APP_NAME}
 
-cf schedule-job ${JOB_NAME} "*/15 * ? * *"
-cf run-job ${JOB_NAME}
+
+function deploy_job() {
+  app=$1
+  job=$2
+  echo "Job $job has not been created yet. Doing so now."
+  cf create-job "${app}" "${job}" ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
+  cf schedule-job "${job}" "*/15 * ? * *"
+  cf run-job "${job}"
+}
+
+cf jobs | grep $JOB_NAME && echo "Job $JOB_NAME has already been created. Skipping creation and scheduling." || deploy_job $APP_NAME $JOB_NAME
